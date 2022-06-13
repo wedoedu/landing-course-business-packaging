105e6df1b26c2961abdbb70eac2728c2e18efda4
Aidos Kabezov <86238646+Aidosio@users.noreply.github.com>
2022-06-13 16:50:09 +0600

    second project

--- src/js/main.js
+++ src/js/main.js
@@ -7,21 +7,21 @@ import '../scss/main.scss'
 new Accordion('.faq-accordion', {})
 
 // .main section
-setInterval(() => {
-  const remaining = new RemainingTime('2022-05-31', false)
+// setInterval(() => {
+//   const remaining = new RemainingTime('2022-05-31', false)
 
-  const dom = {
-    days: document.querySelector('.main-timer-countdown-item.days'),
-    hours: document.querySelector('.main-timer-countdown-item.hours'),
-    minutes: document.querySelector('.main-timer-countdown-item.minutes'),
-    seconds: document.querySelector('.main-timer-countdown-item.seconds'),
-  }
+//   const dom = {
+//     days: document.querySelector('.main-timer-countdown-item.days'),
+//     hours: document.querySelector('.main-timer-countdown-item.hours'),
+//     minutes: document.querySelector('.main-timer-countdown-item.minutes'),
+//     seconds: document.querySelector('.main-timer-countdown-item.seconds'),
+//   }
 
-  dom.days.innerHTML = `${remaining.days}д`
-  dom.hours.innerHTML = `${remaining.hours}ч`
-  dom.minutes.innerHTML = `${remaining.minutes}м`
-  dom.seconds.innerHTML = `${remaining.seconds}с`
-}, 1000)
+//   dom.days.innerHTML = `${remaining.days}д`
+//   dom.hours.innerHTML = `${remaining.hours}ч`
+//   dom.minutes.innerHTML = `${remaining.minutes}м`
+//   dom.seconds.innerHTML = `${remaining.seconds}с`
+// }, 1000)
 
 // .program section
 new Accordion('.program-accordion', {})
